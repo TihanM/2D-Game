e31afa36f0d59144aee64449765d7f6ed52f90b8
TihanM <85705431+TihanM@users.noreply.github.com>
2021-06-11 05:46:12 +0200

    Initial commit

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public Transform spawnPrefab;
+
+    public IEnumerable RespawnPlayer()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds (spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
+        Destroy(clone.gameObject, 3f);
+    }
+
+    public static void KillPlayer (Player player)
+    {
+        Destroy (player.gameObject);
+        gm.RespawnPlayer();
+    }
+}
